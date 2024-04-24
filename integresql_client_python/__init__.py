a9776a4af077b9cb4dc128f497c2148753accbbb
Brian McKeon <135748266+brianjosephmckeon@users.noreply.github.com>
2024-04-23 21:56:32 -0400

    Add debugging statements. (#4)
    Add tpl_name to Integresql class init so a constant template name can be specified.

--- integresql_client_python/__init__.py
+++ integresql_client_python/__init__.py
@@ -3,14 +3,16 @@ __all__ = ["IntegreSQL", "DBInfo", "Database", "Template"]
 import hashlib
 import http.client
 import json
+import logging
 import os
 import pathlib
-import sys
 from typing import List, NoReturn, Optional, Union
 
 import requests
 
 from . import errors
+
+logger = logging.getLogger("integresql-client-python")
 
 __version__ = "0.9.2"
 ENV_INTEGRESQL_CLIENT_BASE_URL = "INTEGRESQL_CLIENT_BASE_URL"
@@ -71,14 +73,14 @@ class TemplateHash:
 
     @classmethod
     def calculate(cls, path: pathlib.Path) -> str:
-        template_hash = hashlib.md5()  # noqa: S303  # nosec
+        template_hash = hashlib.md5()
         items = list(path.rglob("*"))
         items.sort()
         for item in items:
             if item.is_dir():
                 continue
 
-            item_hash = hashlib.md5()  # noqa: S303  # nosec
+            item_hash = hashlib.md5()
             with item.open("rb") as fh:
                 while True:
                     data = fh.read(cls.BUFFER_SIZE)
@@ -96,6 +98,8 @@ class Database:
         self.dbinfo = None
 
     def open(self) -> DBInfo:
+        logger.debug(f"Database: open: {self.dbinfo}")
+
         rsp = self.integresql.request(
             "GET", f"/templates/{self.integresql.tpl_hash}/tests"
         )
@@ -114,6 +118,8 @@ class Database:
             )
 
     def close(self, db_id: Union[int, DBInfo]) -> NoReturn:
+        logger.debug(f"Database: close: {self.dbinfo}")
+
         if isinstance(db_id, DBInfo):
             db_id = db_id.db_id
 
@@ -140,6 +146,8 @@ class Database:
             )
 
     def mark_unmodified(self, db_id: Union[int, DBInfo]) -> NoReturn:
+        logger.debug(f"Database: mark_unmodified: {db_id}")
+
         if isinstance(db_id, DBInfo):
             db_id = db_id.db_id
 
@@ -162,10 +170,14 @@ class Database:
             )
 
     def __enter__(self) -> DBInfo:
+        logger.debug(f"Database: __enter__: {self.dbinfo}")
+
         self.dbinfo = self.open()
         return self.dbinfo
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        logger.debug(f"Database: __exit__: {self.dbinfo}")
+
         self.close(self.dbinfo.db_id)
 
 
@@ -175,6 +187,8 @@ class Template:
         self.dbinfo = None
 
     def initialize(self) -> "Template":
+        logger.debug("Template: initialize")
+
         rsp = self.integresql.request(
             "POST", "/templates", payload={"hash": str(self.integresql.tpl_hash)}
         )
@@ -192,6 +206,8 @@ class Template:
             )
 
     def finalize(self) -> NoReturn:
+        logger.debug(f"Template: finalize: {self.dbinfo}")
+
         rsp = self.integresql.request("PUT", f"/templates/{self.integresql.tpl_hash}")
         if rsp.status_code == http.client.NO_CONTENT:
             return
@@ -206,15 +222,23 @@ class Template:
             )
 
     def discard(self) -> NoReturn:
+        logger.debug(f"Template: discard: {self.dbinfo}")
+
         return self.integresql.discard_template(self.integresql.tpl_hash)
 
     def get_database(self) -> Database:
+        logger.debug(f"Template: get_database: {self.dbinfo}")
+
         return Database(self.integresql)
 
     def __enter__(self) -> DBInfo:
+        logger.debug(f"Template: __enter__: {self.dbinfo}")
+
         return self.dbinfo
 
-    def __exit__(self, exc_type, exc_val, exc_tb):  # noqa
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        logger.debug(f"Template: __exit__: {self.dbinfo}")
+
         self.finalize()
 
 
@@ -227,6 +251,7 @@ class IntegreSQL:
         *,
         base_url: Optional[str] = None,
         api_version: Optional[str] = None,
+        tpl_name: str | None = None,
     ) -> None:
         if not base_url:
             base_url = os.environ.get(
@@ -239,9 +264,8 @@ class IntegreSQL:
 
         self.base_url = base_url
         self.api_version = api_version
-        self.debug = False
         self._connection = None
-        self._tpl_hash = None
+        self._tpl_hash = tpl_name
         if tpl_directory:
             self.tpl_hash = tpl_directory
 
@@ -262,9 +286,13 @@ class IntegreSQL:
         self._tpl_hash = value
 
     def get_template(self) -> Template:
+        logger.debug("IntegreSQL: get_template")
+
         return Template(self)
 
     def discard_template(self, tpl_hash: Union[TemplateHash, str]) -> NoReturn:
+        logger.debug("IntegreSQL: discard_template")
+
         rsp = self.request("DELETE", f"/templates/{tpl_hash}")
         if rsp.status_code == http.client.NO_CONTENT:
             return
@@ -279,6 +307,8 @@ class IntegreSQL:
             )
 
     def reset_all_tracking(self) -> NoReturn:
+        logger.debug("IntegreSQL: reset_all_tracking")
+
         rsp = self.request("DELETE", "/admin/templates")
         if rsp.status_code == http.client.NO_CONTENT:
             return
@@ -300,35 +330,38 @@ class IntegreSQL:
         qs: Optional[dict] = None,
         payload: Optional[dict] = None,
     ) -> requests.Response:
+        logger.debug("IntegreSQL: request")
+
         path = path.lstrip("/")
         url = f"{self.base_url}/{self.api_version}/{path}"
         headers = {"content-type": "application/json"}
         json_payload = json.dumps(payload)
 
-        if self.debug:
-            print(
-                f"Request {method.upper()} to {url} with qs {qs} and headers {headers} and data {json_payload}",
-                file=sys.stderr,
-            )
-
+        logger.info(
+            f"Request {method.upper()} to {url} with qs {qs} and headers {headers} and data {json_payload}"
+        )
         rsp = self.connection.request(method, url, qs, json_payload, headers=headers)
 
-        if self.debug:
-            print(
-                f"Response from {method.upper()} {url}: [{rsp.status_code}] {rsp.content}",
-                file=sys.stderr,
-            )
+        logger.info(
+            f"Response from {method.upper()} {url}: [{rsp.status_code}] {rsp.content}"
+        )
 
         return rsp
 
     def close(self) -> NoReturn:
+        logger.debug("IntegreSQL: close")
+
         self._tpl_hash = None
         if self._connection:
             self._connection.close()
             self._connection = None
 
     def __enter__(self) -> Template:
+        logger.debug("IntegreSQL: __enter__")
+
         return self.get_template()
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> NoReturn:  # noqa
+    def __exit__(self, exc_type, exc_val, exc_tb) -> NoReturn:
+        logger.debug("IntegreSQL: __exit__")
+
         self.close()
